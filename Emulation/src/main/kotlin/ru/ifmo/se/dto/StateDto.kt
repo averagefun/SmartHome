85d86bc8ba6ba48386fcb58a7683cb12ebbd1fb9
averagefun <andrew.valid@yandex.ru>
2024-05-10 23:15:13 +0300

    Emulation: update mock rooms state

--- Emulation/src/main/kotlin/ru/ifmo/se/dto/StateDto.kt
+++ Emulation/src/main/kotlin/ru/ifmo/se/dto/StateDto.kt
@@ -18,25 +18,28 @@ data class RoomDto(
     val id: Long,
     val name: String,
     val type: String,
-    val sensors: List<SensorDto>,
-    val switches: List<SwitchDto>,
-    val rangeSwitches: List<RangeSwitchDto>,
+    val sensors: List<SensorDto> = emptyList(),
+    val switches: List<SwitchDto> = emptyList(),
+    val rangeSwitches: List<RangeSwitchDto> = emptyList(),
 )
 
 data class SensorDto(
     val id: Long,
+    val name: String,
     val type: String,
     val value: Double,
 )
 
 data class SwitchDto(
     val id: Long,
+    val name: String,
     val type: String,
     var enabled: Boolean,
 )
 
 data class RangeSwitchDto(
     val id: Long,
+    val name: String,
     val type: String,
     var enabled: Boolean,
     var value: Double,
--- Emulation/src/main/kotlin/ru/ifmo/se/models/Hub.kt
+++ Emulation/src/main/kotlin/ru/ifmo/se/models/Hub.kt
@@ -6,7 +6,6 @@ import ru.ifmo.se.dto.SensorDto
 import ru.ifmo.se.dto.StateDto
 import ru.ifmo.se.dto.SwitchDto
 import ru.ifmo.se.dto.UpdateDto
-import kotlin.random.Random
 
 class Hub(
     override val hubId: Long,
@@ -19,32 +18,180 @@ class Hub(
 }
 
 val mockState = StateDto(
-    stateId = 0,
+    stateId = 1L,
     rooms = listOf(
         RoomDto(
             id = 1L,
-            name = "roomName",
-            type = "roomType",
+            name = "Улица",
+            type = "outside",
             sensors = listOf(
                 SensorDto(
-                    id = Random.nextLong(1, 1000),
-                    type = "%mock%",
-                    value = Random.nextDouble(1.0, 1000.0)
+                    id = 1L,
+                    name = "Температура",
+                    type = "temperature",
+                    value = 25.0
+                ),
+                SensorDto(
+                    id = 2L,
+                    name = "Влажность",
+                    type = "humidity",
+                    value = 45.0
                 )
             ),
             switches = listOf(
-                SwitchDto(id = Random.nextLong(1, 1000), type = "%mock%", enabled = true)
+                SwitchDto(
+                    id = 1L,
+                    name = "Уличное освещение",
+                    type = "light",
+                    enabled = false
+                )
+            )
+        ),
+        RoomDto(
+            id = 2L,
+            name = "Дом",
+            type = "home",
+            sensors = listOf(
+                SensorDto(
+                    id = 3L,
+                    name = "Средняя температура",
+                    type = "temperature",
+                    value = 24.0
+                ),
+                SensorDto(
+                    id = 4L,
+                    name = "Общее энергопотребление",
+                    type = "power",
+                    value = 70.0
+                )
+            ),
+            switches = listOf(
+                SwitchDto(
+                    id = 2L,
+                    name = "WiFi",
+                    type = "power",
+                    enabled = false
+                ),
+                SwitchDto(
+                    id = 3L,
+                    name = "Входная дверь",
+                    type = "lock",
+                    enabled = true
+                )
+            )
+        ),
+        RoomDto(
+            id = 3L,
+            name = "Прихожая",
+            type = "hallway",
+            sensors = listOf(
+                SensorDto(
+                    id = 5L,
+                    name = "Температура в прихожей",
+                    type = "temperature",
+                    value = 23.0
+                )
+            ),
+            switches = listOf(
+                SwitchDto(
+                    id = 4L,
+                    name = "Освещение в прихожей",
+                    type = "light",
+                    enabled = false
+                )
+            )
+        ),
+        RoomDto(
+            id = 4L,
+            name = "Гостиная",
+            type = "living",
+            sensors = listOf(
+                SensorDto(
+                    id = 6L,
+                    name = "Температура в гостиной",
+                    type = "temperature",
+                    value = 22.3
+                ),
+                SensorDto(
+                    id = 7L,
+                    name = "Температура в аквариуме",
+                    type = "temperature",
+                    value = 26.1
+                )
             ),
             rangeSwitches = listOf(
                 RangeSwitchDto(
-                    id = Random.nextLong(1, 1000),
-                    type = "%mock%",
+                    id = 1L,
+                    name = "Кондиционер",
+                    type = "climat",
+                    enabled = false,
+                    value = 23.1,
+                    minValue = 15.0,
+                    maxValue = 28.0
+                )
+            )
+        ),
+        RoomDto(
+            id = 5L,
+            name = "Спальня",
+            type = "living",
+            sensors = listOf(
+                SensorDto(
+                    id = 8L,
+                    name = "Температура в спальне",
+                    type = "temperature",
+                    value = 22.3
+                )
+            ),
+            rangeSwitches = listOf(
+                RangeSwitchDto(
+                    id = 2L,
+                    name = "Освещение",
+                    type = "light",
                     enabled = true,
-                    value = Random.nextDouble(1.0, 100.0),
-                    minValue = 1.0,
+                    value = 40.0,
+                    minValue = 0.0,
                     maxValue = 100.0
                 )
             )
-        )
+        ),
+        RoomDto(
+            id = 6L,
+            name = "Ванная",
+            type = "bathroom",
+            sensors = listOf(
+                SensorDto(
+                    id = 9L,
+                    name = "Температура в ванной",
+                    type = "temperature",
+                    value = 25.1
+                ),
+                SensorDto(
+                    id = 10L,
+                    name = "Влажность в ванной",
+                    type = "humidity",
+                    value = 68.0
+                )
+            ),
+        ),
+        RoomDto(
+            id = 7L,
+            name = "Кухня",
+            type = "kitchen",
+            switches = listOf(
+                SwitchDto(
+                    id = 5L,
+                    name = "Розетка (чайник)",
+                    type = "power",
+                    enabled = false
+                ),
+                SwitchDto(
+                    id = 6L,
+                    name = "Розетка (холодильник)",
+                    type = "power",
+                    enabled = true
+                )
+            )
+        ),
     )
 )
